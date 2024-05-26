c08d37c2ee9f2f2ad8bfbdaa5991e45ad2f6474a
uet20020715vnu <quangthai3110@gmail.com>
2024-05-26 23:47:38 +0700

    delete file

--- src/main/java/com/example/ecommercebe/projection/UserProjection.java
+++
@@ -1,4 +0,0 @@
-package com.example.ecommercebe.projection;
-
-public class UserProjection {
-}
--- src/main/java/com/example/ecommercebe/statics/enums/ERole.java
+++
@@ -1,7 +0,0 @@
-package com.example.ecommercebe.statics.enums;
-
-public enum ERole {
-    ROLE_USER,
-    ROLE_MODERATOR,
-    ROLE_ADMIN
-}
