ca3090e065c6ce1b4a71f234f76de509a52cec21
uet20020715vnu <quangthai3110@gmail.com>
2024-05-27 16:14:19 +0700

    Update InStockMapper.java

--- src/main/java/com/example/ecommercebe/mapper/InStockMapper.java
+++ src/main/java/com/example/ecommercebe/mapper/InStockMapper.java
@@ -7,8 +7,8 @@ import com.example.ecommercebe.entities.StockId;
 public class InStockMapper {
     public static InStockDTO toDTO(InStock inStock){
         InStockDTO inStockDTO = new InStockDTO();
-        inStockDTO.setProduct_id(inStock.getId().getProductId());
-        inStockDTO.setClinic_id(inStock.getId().getClinicId());
+        inStockDTO.setProduct_id(inStock.getProductId());
+        inStockDTO.setClinic_id(inStock.getClinicId());
         inStockDTO.setStockQuantity(inStock.getStockQuantity());
         inStockDTO.setLastUpdate(inStock.getLastUpdated());
         inStockDTO.setStatusStock(inStock.getStockStatus());
@@ -17,7 +17,8 @@ public class InStockMapper {
 
     public static InStock toEntity(InStockDTO inStockDTO){
         InStock inStock = new InStock();
-        inStock.setId(new StockId(inStockDTO.getProduct_id(), inStockDTO.getClinic_id()));
+        inStock.setProductId(inStockDTO.getProduct_id());
+        inStock.setClinicId(inStockDTO.getClinic_id());
         inStock.setStockStatus(inStockDTO.getStatusStock());
         inStock.setStockQuantity(inStockDTO.getStockQuantity());
         inStock.setLastUpdated(inStockDTO.getLastUpdate());
